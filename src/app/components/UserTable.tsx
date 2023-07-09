2c9ddec173e4e906569b58b290c98e9d9d231f0e
rithik56 <rithik19901998@gmail.com>
2023-07-09 18:49:32 +0530

    GAM-12: Added Top User functionality

--- src/app/components/UserTable.tsx
+++ src/app/components/UserTable.tsx
@@ -1,5 +1,9 @@
-import styles from '../styles/usertable.module.css'
+'use client'
 
+import styles from '../styles/usertable.module.css'
+import { useState } from 'react'
+
+// user object type
 type usersType = {
     id: number,
     name: string,
@@ -24,11 +28,47 @@ type usersType = {
     }
 }
 
+// props interface
 interface Props {
     users: usersType[]
 }
 
 export default function UserTable({ users }: Props) {
+
+    const [topUsers, setTopUsers] = useState(() => {
+        // Fetching the top users from the local storage
+        let topUsersKey = localStorage.getItem('top_users')
+        // checking if top_users exists
+        if (topUsersKey) {
+            // returning the top_users array from local storage
+            return JSON.parse(topUsersKey)
+        }
+        else {
+            // returning null as top_users doesn't exist
+            return null
+        }
+    })
+
+    function addTopUser(id: number) {
+
+        if (topUsers) {
+            // if top users array is present in local storage, then we are appending the value to top users array
+            localStorage.setItem('top_users', JSON.stringify([...topUsers, id]))
+            setTopUsers([...topUsers, id])
+        }
+        else {
+            // if top users array is not present in local storage, then we are setting the top users
+            localStorage.setItem('top_users', JSON.stringify([id]))
+            setTopUsers([id])
+        }
+    }
+
+    function removeTopUser(id: number) {
+        // Filtered the array and return the array in which id is not present
+        localStorage.setItem('top_users', JSON.stringify(topUsers.filter((curr: number) => curr !== id)))
+        setTopUsers(topUsers.filter((curr: number) => curr !== id))
+    }
+
     return (
         <>
             <table className={styles.table}>
@@ -51,7 +91,19 @@ export default function UserTable({ users }: Props) {
                                     </label>
                                 </td>
                                 <td className={styles.td}>
-                                    <input type='checkbox' className={styles.checkbox} />
+                                    <input
+                                        type='checkbox'
+                                        className={styles.checkbox}
+                                        checked={(topUsers && topUsers.includes(user.id)) ? true : false}
+                                        onChange={(e) => {
+                                            if (e.target.checked) {
+                                                addTopUser(user.id)
+                                            }
+                                            else {
+                                                removeTopUser(user.id)
+                                            }
+                                        }}
+                                    />
                                 </td>
                             </tr>
                         )
