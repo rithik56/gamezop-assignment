a7caf594ff6a0e41866a6077f0664982127e2f39
rithik56 <rithik19901998@gmail.com>
2023-07-10 21:13:49 +0530

    GAM-21: Display user information in top users page

--- src/app/components/TopUsersTable.tsx
+++ src/app/components/TopUsersTable.tsx
@@ -2,7 +2,8 @@
 
 import styles from '../styles/table.module.css'
 import SearchBar from './SearchBar'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import UserDetails from './UserDetails'
 
 export default function TopUsersTable() {
     const topUsersKey = localStorage.getItem('top_users')
@@ -10,9 +11,18 @@ export default function TopUsersTable() {
     if (topUsersKey) {
         users = Object.values(JSON.parse(topUsersKey))
     }
+
     const [filteredUsers, setFilteredUsers] = useState<usersType[] | null>(null)
+
     const [category, setCategory] = useState<'name' | 'email'>('name')
+
     const [query, setQuery] = useState('')
+
+    const [showUserDetails, setShowUserDetails] = useState<{ showUser: boolean, userDetails: null | usersType }>({
+        showUser: false,
+        userDetails: null
+    })
+
     function filterDataHandler(category: "name" | "email", query: string) {
 
         let newData: usersType[] | null
@@ -40,30 +50,35 @@ export default function TopUsersTable() {
     }
     return (
         <>
-            <>
-                <SearchBar
-                    category={category}
-                    query={query}
-                    filterUserHandler={(category, query) => filterDataHandler(category, query)}
-                    filteredUsers={filteredUsers}
-                />
-                <table className={styles.table}>
-                    <tbody>
-                        <tr className={`${styles.tr} ${styles.header_row}`} key={0}>
-                            <th className={styles.th}>Name</th>
-                            <th className={styles.th}>Email</th>
-                        </tr>
-                        {users && users.map((user: usersType) => {
-                            return (
-                                <tr className={styles.tr} key={user.id}>
-                                    <td className={styles.td}>{user.name}</td>
-                                    <td className={styles.td}>{user.email}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            </>
+            <SearchBar
+                category={category}
+                query={query}
+                filterUserHandler={(category, query) => filterDataHandler(category, query)}
+                filteredUsers={filteredUsers}
+                updateShowUserHandler={(showUser: boolean, userDetails: usersType) => {
+                    setShowUserDetails({
+                        showUser: showUser,
+                        userDetails: userDetails
+                    })
+                    setQuery('')
+                }}
+            />
+            {!showUserDetails.showUser ? <table className={styles.table}>
+                <tbody>
+                    <tr className={`${styles.tr} ${styles.header_row}`} key={0}>
+                        <th className={styles.th}>Name</th>
+                        <th className={styles.th}>Email</th>
+                    </tr>
+                    {users && users.map((user: usersType) => {
+                        return (
+                            <tr className={styles.tr} key={user.id}>
+                                <td className={styles.td}>{user.name}</td>
+                                <td className={styles.td}>{user.email}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table> : showUserDetails.userDetails && <UserDetails user={showUserDetails.userDetails} />}
         </>
     )
 }
